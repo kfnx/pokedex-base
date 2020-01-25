602040ffddadb8f6a3d24a3743eb608b6154750a
Kafin Salim <kafinsalim@gmail.com>
2020-01-25 23:49:18 +0700

    🚀

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import s from "./style.css";
+
+class MyComponent extends Component {
+  render() {
+    return <div className={s.intro}>Hello World</div>;
+  }
+}
+
+export default MyComponent;
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "./main.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true
+  }
+};
